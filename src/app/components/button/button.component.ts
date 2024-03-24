fea955a41dfb4332bbb7573a3e9752540e511166
Eric <throwawayehcksdee@gmail.com>
2024-03-24 13:02:06 -0400

    button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, computed, EventEmitter, input, Output } from '@angular/core';
+
+type ButtonSize = "large" | "small"
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
+  text = input('')
+  size = input<ButtonSize>('small')
+  className = computed(() => `btn-${this.size()}`)
+  
+  @Output() clicked = new EventEmitter() 
 
+  onClicked(event: Event) {
+    this.clicked.emit(event)
+  }
 }
--- src/app/forms/login-form/login-form.component.ts
+++ src/app/forms/login-form/login-form.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import models from '../../models';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ButtonComponent],
   templateUrl: './login-form.component.html',
   styleUrl: '../form.component.css'
 })
