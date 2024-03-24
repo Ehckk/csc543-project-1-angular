3b05c51f1c35b0773a1713ce6f1b1dc212934e8e
Eric <throwawayehcksdee@gmail.com>
2024-03-24 14:27:44 -0400

    registration form

--- src/app/forms/register-form/register-form.component.ts
+++ src/app/forms/register-form/register-form.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import models from '../../models';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { ButtonComponent } from '../../components/button/button.component';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ButtonComponent, RouterLink],
   templateUrl: './register-form.component.html',
   styleUrl: '../form.component.css'
 })
 export class RegisterFormComponent {
   model = new models.Registration()
+  user = inject(UserService)
+  submitted = false
+
+  constructor() {}
+
+  onSubmit() {
+    console.log(this.model);
+    this.submitted = true
+    this.user.register(this.model)
+  }
 }
